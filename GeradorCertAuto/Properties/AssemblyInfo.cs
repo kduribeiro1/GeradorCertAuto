6fa5b7c7a0a700be833401d8f29161defdcd1712
Kdu Ribeiro <32025410+kduribeiro1@users.noreply.github.com>
2025-07-28 22:43:30 -0300

    Ajustes Finais

--- GeradorCertAuto/Properties/AssemblyInfo.cs
+++ GeradorCertAuto/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Security;
 // É possível especificar todos os valores ou usar como padrão os Números da Versão e da Revisão 
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.13")]
+[assembly: AssemblyFileVersion("1.0.0.13")]
 
