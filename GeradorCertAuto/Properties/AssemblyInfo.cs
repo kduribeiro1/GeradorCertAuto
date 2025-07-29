cb23f814294f49683ae148d2f93bba42589da7a0
Kdu Ribeiro <32025410+kduribeiro1@users.noreply.github.com>
2025-07-28 21:07:19 -0300

    Ajustes na versão

--- GeradorCertAuto/Properties/AssemblyInfo.cs
+++ GeradorCertAuto/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Security;
 [assembly: AssemblyTitle("GeradorCertAuto")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kdu Ribeiro")]
 [assembly: AssemblyProduct("GeradorCertAuto")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
@@ -33,6 +33,6 @@ using System.Security;
 // É possível especificar todos os valores ou usar como padrão os Números da Versão e da Revisão 
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
 
